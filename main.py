b15f8c8a401774dad8a402558841f7815b91a267
DESKTOP-ANTON\utzan <alien-code@outlook.de>
2022-04-28 10:03:50 +0200

    commented a little bit

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from Link import Link
 from Node import Node
 
 
+# check input arguments on correctness, min. 2 args given? file given is a .txt?
 def check_input():
     if len(sys.argv) <= 2:
         print("Error: Arguments missing. Usage: [filename].py [importfile].txt [Number for cycles]")
@@ -14,15 +15,17 @@ def check_input():
         sys.exit()
 
 
+# read given import file, create node and link objects from input, return list of nodes to main
 def read_import_file():
     list_of_nodes = []
     list_of_links = []
     with open(sys.argv[1], 'r') as f:
         first_line = f.readline()
+        # first line is the title of the graph
         if re.search("Graph [a-z|A-Z]*{\n", first_line):
-            title_of_graph = first_line[5:len(first_line)-2]
+            title_of_graph = first_line[5:len(first_line) - 2]
         else:
-            print("Error: file format of input file wrong")
+            print("Error: no title graph title given or file format of input file wrong")
             sys.exit()
         for line in f:
             # to find Node in import file
@@ -30,20 +33,31 @@ def read_import_file():
                 list_of_nodes.append(Node(line[8], line[4]))
             # to find links in import file
             elif re.search("[a-z|A-Z] - [a-z|A-Z] : [0-9]{1,3};", line):
-                number = int(line[12:len(line)-2])
+                number = int(line[12:len(line) - 2])
                 start = ([i for i in list_of_nodes if i.name == line[4]][0])
                 end = ([i for i in list_of_nodes if i.name == line[8]][0])
                 list_of_links.append(Link(start, end, number))
-#        print([k.printout_object() for k in list_of_links])
-#        print([k.printout_object() for k in list_of_nodes])
+        # add links to the node.list_of_links
         for node in list_of_nodes:
             for link in list_of_links:
                 if node == link.start or node == link.end:
                     node.list_of_links.append(link)
-#        print([k.printout_object() for k in list_of_nodes[2].list_of_links])
     return list_of_nodes, title_of_graph
 
 
+# check the created graph on some criteria, abort program if error found
+def check_read_graph(list_of_nodes):
+    for node in list_of_nodes:
+        if len(node.list_of_links) == 0:
+            print("Error: found node without link to graph")
+            sys.exit()
+        for link in node.list_of_links:
+            if link.start == link.end:
+                print("Error: link to self found")
+                sys.exit()
+
+
+# if graph has been calculated, print the result with this function
 def output(list_of_nodes, title_of_graph):
     output_string = "Results for Graph: " + title_of_graph + "\n\n// NodeX: Use NodeY as Next Hop to Root \n\n"
     for k in list_of_nodes:
@@ -57,6 +71,7 @@ def output(list_of_nodes, title_of_graph):
 def main():
     check_input()
     list_of_nodes, title_of_graph = read_import_file()
+    check_read_graph(list_of_nodes)
     for i in range(int(sys.argv[2])):
         for k in list_of_nodes:
             k.send_message()
